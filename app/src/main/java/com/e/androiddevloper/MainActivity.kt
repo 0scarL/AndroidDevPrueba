8165bfccf2488afce7a5a4b92063fc044a39e2fa
0scarL <o_lizarazob@hotmail.com>
2020-11-21 19:12:09 -0500

    Se realiza prueba de adquisicion de datos desde Api JsonPlaceHolder ok!

--- app/src/main/java/com/e/androiddevloper/MainActivity.kt
+++ app/src/main/java/com/e/androiddevloper/MainActivity.kt
@@ -2,13 +2,40 @@ package com.e.androiddevloper
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.e.androiddevloper.Model.PostDbClient
+import com.e.androiddevloper.Model.apis.PostDbService
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
 import retrofit2.Retrofit
+import retrofit2.create
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        testDataFromPost()
+
+    }
+
+    fun testDataFromPost() {
+        doAsync {
+            val call = PostDbClient.service()
+                    .create(PostDbService::class.java)
+                    .getPost()
+                    .execute()
+            uiThread {
+                if (call.isSuccessful) {
+                    Toast.makeText(it, "se cargaron los datos ", Toast.LENGTH_SHORT).show()
+                    val llamado = call.body().toString()
+                    Toast.makeText(it, "" + llamado, Toast.LENGTH_SHORT).show()
+                    textView.text = call.body().toString()
+                }
+
+            }
+        }
     }
 
 }
